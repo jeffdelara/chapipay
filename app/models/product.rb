1cdb3ad5be609e5e7fe2e91a071ecfc11fe301bf
Jeffrey de Lara <jeffreytdelara@gmail.com>
2022-04-12 18:24:44 +0800

    Upload multiple product images

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,5 +2,8 @@ class Product < ApplicationRecord
   
   belongs_to :category
   validates :name, :price, :category_id, presence: true 
+  has_many_attached :images do |attachable|
+    attachable.variant :thumb, resize_to_limit: [100, 100]
+  end
 
 end
