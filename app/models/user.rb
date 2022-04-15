b2504a477a935c77f4de8913604c5bebd41830fc
Germaine Mojica <germaine.mojica@gmail.com>
2022-04-15 23:37:49 +0800

    Generate address model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :addresses
+
   def customer? 
     !self.admin?
   end
