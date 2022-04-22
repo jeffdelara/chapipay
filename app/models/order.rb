54d664ab4cd72a58e0749a2021996bddd2f027fc
Jeffrey de Lara <jeffreytdelara@gmail.com>
2022-04-22 10:35:22 +0800

    refactored order statuses

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,8 +3,10 @@ class Order < ApplicationRecord
   belongs_to :user
   has_many :order_lines
 
-
   validates :customer_details, presence: true
   validates :delivery_details, presence: true
   validates :status, presence: true
+
+  STATUSES = ['pending', 'processing', 'completed']
+
 end
