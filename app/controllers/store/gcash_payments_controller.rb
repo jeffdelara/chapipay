d6d408a482475f0237827259b565116c7c5a18e9
Germaine Mojica <germaine.mojica@gmail.com>
2022-04-29 14:25:28 +0800

    Get address id for order and create failed method

--- app/controllers/store/gcash_payments_controller.rb
+++ app/controllers/store/gcash_payments_controller.rb
@@ -11,7 +11,7 @@ class Store::GcashPaymentsController < ApplicationController
       session[:status] = src_status
       session[:gcash_src_id] = src_id
 
-      p gcash_params
+      session[:address_id] = params[:address_id]
 
       if src_status == 'pending'
         redirect_to response[:checkout_url]
@@ -25,19 +25,23 @@ class Store::GcashPaymentsController < ApplicationController
     retrieve_src_amount = response_src[:amount]
 
     response_pay = Payment::PaymongoGcash.make_payment(retrieve_src_amount, session[:gcash_src_id])
-    # transaction_id = response_pay[:id]
+    transaction_id = response_pay[:id]
 
-    # unless Order.find_by(transaction_id: transaction_id)
-    #   order = Orders::CreateOrder.run(
-    #     current_user.id, 
-    #     session[:address_id],
-    #     transaction_id, 
-    #     'paid'
-    #   )
+    unless Order.find_by(transaction_id: transaction_id)
+      order = Orders::CreateOrder.run(
+        current_user.id, 
+        session[:address_id],
+        transaction_id, 
+        'paid'
+      )
 
-    #   # clear cart
-    #   Cart::CartService.remove_all(current_user)
-    #   redirect_to customer_order_path(order), notice: 'Payment successful!'
-    # end
+      # clear cart
+      Cart::CartService.remove_all(current_user)
+      redirect_to customer_order_path(order), notice: 'Payment successful!'
+    end
+  end
+
+  def failed
+    redirect_to root_path, alert: 'Payment failed! Please try again.'
   end
 end
